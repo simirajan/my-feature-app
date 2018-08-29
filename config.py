c2f8787d66fd469baf1479ff5464c688a891d6c3
simirajan <42344516+simirajan@users.noreply.github.com>
2018-08-29 15:51:33 +0400

    Update config.py

--- config.py
+++ config.py
@@ -1,9 +1,4 @@
-# edit the URI below to add your RDS password and your AWS URL
 
-# SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://(user):(password)@(db_endpoint)/(db_name)'
-
-# Comment the line below if you want to work with a RDS account. Keep it as is for a local DB.
-#SQLALCHEMY_DATABASE_URI = 'sqlite:////tmp/test2.db'
 SQLALCHEMY_DATABASE_URI = 'sqlite:///tmp/test2.db'
 
 
