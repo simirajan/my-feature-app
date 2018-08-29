e7d6cc0a3c68d10b1da5e0b51d276b3e24153cc4
simi.rajan <simi.rajan90@gmail.com>
2018-08-29 17:04:22 +0400

    changes

--- config.py
+++ config.py
@@ -1,16 +1,5 @@
-<<<<<<< HEAD
 
 SQLALCHEMY_DATABASE_URI = 'sqlite:///C:\\sqlite\\tmp\\test2.db'
 
 WTF_CSRF_ENABLED = True
 SECRET_KEY = 'dsaf0897sfdg45sfdgfdsaqzdf98sdf0a'
-||||||| merged common ancestors
-=======
-
-SQLALCHEMY_DATABASE_URI = 'sqlite:///tmp/test2.db'
-
-
-
-WTF_CSRF_ENABLED = True
-SECRET_KEY = 'dsaf0897sfdg45sfdgfdsaqzdf98sdf0a'
->>>>>>> c2f8787d66fd469baf1479ff5464c688a891d6c3
