325c341b408a4a5275e8e30a9cff84c5df173218
simi.rajan <simi.rajan90@gmail.com>
2018-09-02 17:52:42 +0400

    updated

--- config.py
+++ config.py
@@ -1,7 +1,5 @@
 
 SQLALCHEMY_DATABASE_URI = 'sqlite:///C:\\sqlite\\tmp\\test2.db'
 
-WTF_CSRF_ENABLED = True
-SECRET_KEY = 'dsaf0897sfdg45sfdgfdsaqzdf98sdf0a'
 
 #SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:root1234@featureappdbinstance.cdquardnmeru.us-east-2.rds.amazonaws.com/featurerequests'
